5b895c99e1436f70d10a7ef49780eb89afbac74b
Ryan Coates <ryan.coates@gmail.com>
2018-09-25 19:05:02 -0600

    first RPS attempt

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RockPaperScissors
 {
@@ -6,7 +7,119 @@ namespace RockPaperScissors
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var rnd = new Random();
+            var playing = true;
+            var winCount = 0;
+
+            while (playing)
+            {
+                if (winCount == 5)
+                {
+                    Console.Clear();
+                    Console.WriteLine("You have won 5 games, you win!");
+                    Console.ReadLine();
+                    break;
+                }
+
+                var ourChoice = rnd.Next(1, 4);
+
+                Console.WriteLine("Rock, Paper, Scissors?: ");
+                var playerChoice = Console.ReadLine();
+                playerChoice = playerChoice.ToLower();
+                var validChoices = new HashSet<string>();
+                validChoices.Add("rock");
+                validChoices.Add("paper");
+                validChoices.Add("scissors");
+
+                if (validChoices.Contains(playerChoice))
+                {
+                    if (ourChoice == 1)
+                    {
+                        if (playerChoice == "rock")
+                        {
+                            Console.WriteLine("[{0}] Computer chose Rock, Player chose Rock, it is a tie", winCount);
+                            continue;
+                        }
+
+                        if (playerChoice == "paper")
+                        {
+                            Console.WriteLine("[{0}] Computer chose Rock, Player chose Paper, Player wins!", winCount);
+                            winCount++;
+                            continue;
+
+
+                        }
+                        if (playerChoice == "scissors")
+                        {
+                            Console.WriteLine("[{0}] Computer chose Rock, Player chose Sciessors, Computer wins!", winCount);
+                            continue;
+
+                        }
+                    }
+
+                    if (ourChoice == 2)
+                    {
+                        if (playerChoice == "rock")
+                        {
+                            Console.WriteLine("[{0}] Computer chose Paper, Player chose Rock, Computer wins!", winCount);
+                            continue;
+
+                        }
+
+                        if (playerChoice == "paper")
+                        {
+                            Console.WriteLine("[{0}] Computer chose Paper, Player chose Paper, it's a tie", winCount);
+                            continue;
+
+
+                        }
+                        if (playerChoice == "scissors")
+                        {
+                            Console.WriteLine("[{0}] Computer chose Paper, Player chose Scissors, Player wins!", winCount);
+                            winCount++;
+                            continue;
+
+
+                        }
+                    }
+
+                    if (ourChoice == 3)
+                    {
+                        if (playerChoice == "rock")
+                        {
+                            Console.WriteLine("[{0}] Computer chose Scissors, Player chose Rock, Player wins!", winCount);
+                            winCount++;
+                            continue;
+
+                        }
+
+                        if (playerChoice == "paper")
+                        {
+                            Console.WriteLine("[{0}] Computer chose Scissors, Player chose Paper, Computer Wins!", winCount);
+                            continue;
+
+
+                        }
+                        if (playerChoice == "scissors")
+                        {
+                            Console.WriteLine("[{0}] Computer chose Scissors, Player chose Scissors, it's a tie", winCount);
+                            continue;
+
+
+                        }
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("You did not select Rock, Paper or Scissors, try again");
+                    continue;
+                }
+
+
+
+            }
         }
     }
 }
